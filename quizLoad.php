2c571023d69d05acf45d4ac8950e5aaf2e47e8a1
leedahw <71396271+leedahw@users.noreply.github.com>
2020-12-02 17:18:19 -0500

    created quizLoad postAnswers php files. sucesfully gets nad inserts data

--- quizLoad.php
+++ quizLoad.php
@@ -1,12 +1,25 @@
 <?php
+//start session
+session_start();
+
 include('includes/dbconfig.php');
 //GET quiz for quizId==1
 
+//receive input
+$quizId = 1;
+//$quizId = $_POST["quizId"]; //may change to GET. check on
+
+
 $stmt = $pdo->prepare("SELECT * FROM `quizquestion`
-WHERE `quizId` = '1'");
+WHERE `quizId` = '$quizId'");
 
 $stmt->execute();
 
-$row = $stmt->fetch(PDO::FETCH_ASSOC);
-
+while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {  
+    echo($row["questionId"]);
+    echo($row["quizId"]);
+    echo($row["questionString"]);
+    echo($row["resultOption1"]);
+    echo($row["resultOption2"]);
+}
 ?>
