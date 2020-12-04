064bf1a98b21bf758b9f914d2f0ed1b1d69f1868
thisioke <seyitanoke@gmail.com>
2020-12-04 16:09:28 -0500

    get and edit user details.

--- getUserDetails.php
+++ getUserDetails.php
@@ -1,5 +1,5 @@
 <?php
-//updates user details in user table 
+//gets user details in usertabe
 //start session
 session_start();
 
@@ -13,7 +13,15 @@ $password = "87654321";//$_POST["password"];
 //connect to db
 include('includes/dbconfig.php');
 
-$stmt = $pdo->prepare("UPDATE `user` SET `firstName`= '$firstName',`lastName`= '$lastName',`emailAddress`= '$emailAddress',`password`= '$password' WHERE `userId` = '$userId' ");
+$stmt = $pdo->prepare("SELECT * FROM `user` WHERE `userId` = '$userId' ");
 
 $stmt->execute();
+
+while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {  
+    
+    echo($row["firstName"]);//users first name
+    echo($row["lastName"]); //users last name
+    echo($row["emailAddress"]); //users email address
+    echo($row["password"]);//user password
+}
 ?>
