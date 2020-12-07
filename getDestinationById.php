1e3320257fb0a915cf8fa8557673cd88edcad1bf
leedahw <71396271+leedahw@users.noreply.github.com>
2020-12-07 18:55:26 -0500

    repushign ById

---
+++ getDestinationById.php
@@ -0,0 +1,26 @@
+<?php 
+
+//start session
+session_start();
+
+$destinationId = $_GET["destinationId"];
+
+include("includes/dbconfig.php");
+
+$stmt = $pdo->prepare("SELECT * FROM `destination`
+WHERE `destinationId` = '$destinationId'");
+
+$stmt->execute();
+
+$row = $stmt->fetch(PDO:: FETCH_ASSOC);
+    echo ($row["imageLink"]);  //<<needs to be set into img tag
+
+    //destination info
+    echo ($row["destinationName"]);
+    echo ($row["destinationDescription"]);
+    echo ($row["city"]);
+    echo ($row["country"]);
+    echo ($row["imageLink"]);
+
+
+?>
