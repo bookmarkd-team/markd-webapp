47a1c6cb7efaeda2f369ae827f70cc035e08ff13
thisioke <seyitanoke@gmail.com>
2020-12-07 14:40:57 -0500

    Load quiz and submit, and post answers php

--- quizLoad.php
+++ quizLoad.php
@@ -6,20 +6,22 @@ include('includes/dbconfig.php');
 //GET quiz for quizId==1
 
 //receive input
-$quizId = 1;
-//$quizId = $_POST["quizId"]; //may change to GET. check on
+$quizId = 1; //Had code to pull questions for quiz 1) 
+//$quizId = $_SESSION["quizId"]; //may change to GET. check on
 
 
-$stmt = $pdo->prepare("SELECT * FROM `quizquestion`
+$stmtQuizLoad = $pdo->prepare("SELECT * FROM `quizquestion`
 WHERE `quizId` = '$quizId'");
 
-$stmt->execute();
+$stmtQuizLoad->execute();
 
-while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {  
-    echo($row["questionId"]);
-    echo($row["quizId"]);
-    echo($row["questionString"]);
-    echo($row["resultOption1"]);
-    echo($row["resultOption2"]);
-}
+
+////While loop to test echo that thie select statment is working
+// while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {  
+//     echo($row["questionId"]);
+//     echo($row["quizId"]);
+//     echo($row["questionString"]);
+//     echo($row["resultOption1"]);
+//     echo($row["resultOption2"]);
+// }
 ?>
