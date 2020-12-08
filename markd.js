7a548b268a0c9efeba23971d5dcfb7c18f0e5bf0
thisioke <seyitanoke@gmail.com>
2020-12-08 16:21:53 -0500

    more js work

--- markd.js
+++ markd.js
@@ -3,15 +3,20 @@
 
 // add an event listener 
 
+//some varibale that tracks if destination is currently saved and changes front end accrodingly 
+
 // call function to add selected destination to save DB 
     //{
         //if( destination is not saved already ){
          // save it with saveDestinationProccessing.php 
         //} else {
+        
+        // remove it from Save Data base
 
-            
+
         //}
-         
+        
+        
 
 
     //}
