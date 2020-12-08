8cbaf237cd3f3d51cc9e238fdfd9a7d08efa15f2
scdoja <doja@sheridancollege.ca>
2020-12-07 23:31:51 -0500

    Update register-processing.php

--- register-processing.php
+++ register-processing.php
@@ -10,11 +10,11 @@ $password = $_POST["password"];
 //this part adds a new user to the 'user' table
 include('includes/dbconfig.php');
 
-$stmt = $pdo->prepare("INSERT INTO `user` 
-	(`userId`, `firstName`, `lastName`, `emailAddress`, `password`, `created_at`) 
+$stmt = $pdo->prepare("INSERT INTO `user`
+	(`userId`, `firstName`, `lastName`, `emailAddress`, `password`, `created_at`)
 	VALUES (NULL, '$firstName', '$lastName', '$emailAddress', '$password', NULL);");
 
 $stmt->execute();
 ?>
 <p>Thank you for signing up!</p>
-<a href="login.php">Go to Login</a>
+<a href="sign-in.php">Go to Login</a>
