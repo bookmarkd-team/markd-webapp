da579b32d5df82ba8a02951306ffca442c6dff1f
You-you <shinyou_hou@hotmail.com>
2020-12-09 14:13:16 -0500

    editprofile css fixed

--- editProfile.php
+++ editProfile.php
@@ -1,5 +1,7 @@
 <!-- Allo edit of user name, password and email -->
-
+<?php
+include "navheader.html"; 
+?> 
 <!DOCTYPE html>
 <html>
 <head>
@@ -16,13 +18,13 @@
 
 <div id="form">
 <label id="title"> Your Profile </label></br> 
-First Name:<input type="text" id="fName" placeholder="New First Name"> </br>
-Last Name:<input type="text" id="lName" placeholder="New Last Name" > </br>
-Username:<input type="text" id="username" placeholder="New Username" ></br>
-Password:<input type="password" id="password" placeholder="New Password"> </br>
+<label>First Name:</label><input type="text" id="fName" placeholder="New First Name"> </br>
+<label>Last Name:</label><input type="text" id="lName" placeholder="New Last Name" > </br>
+<label>Username:</label><input type="text" id="username" placeholder="New Username" ></br>
+<label>Password:</label><input type="password" id="password" placeholder="New Password"> </br>
 
-<button type="submit" value="update"  class="button">Update Personal Info</button></br>
-<button type="submit" value="reset"  class="button">Reset Password</button>
+<button type="submit" value="update"  class="button">Update Account Info</button></br>
+
 </div>
 </div>
 
