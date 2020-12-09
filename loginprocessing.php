0c0de43c3c2517ed4ea9eea5a19bfae73a310c4d
You-you <shinyou_hou@hotmail.com>
2020-12-09 14:39:25 -0500

    fixed login processing page

--- loginprocessing.php
+++ loginprocessing.php
@@ -4,6 +4,7 @@ session_start();
 //processing the login
 
 //receive input
+include "navheader.html"; 
 $emailAddress = $_POST["emailAddress"];
 $password = $_POST["password"];
 
@@ -16,18 +17,42 @@ WHERE `emailAddress` = '$emailAddress' AND `password` = '$password'");
 
 $stmt->execute();
 
-$row = $stmt->fetch(PDO::FETCH_ASSOC);
+$row = $stmt->fetch(PDO::FETCH_ASSOC); 
+?> 
 
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="css/login-processing.css"> 
+</head>
+
+<body>
+<?php
 if($row){
     //session declarations
     $_SESSION["userId"] = $row["userId"];
 	//successful login
-	?><p>Welcome! Take the Travel Quiz</p> <!-- UPDATED for demo run, change back to "Welcome Back" after Tuesday -->
-	<a href="quiz.php">Go to Travel Quiz</a><?php
-
+	?>
+	<div class="login-form"> 
+	<p>Welcome back! Take the Travel Quiz</p> <!-- UPDATED for demo run, change back to "Welcome Back" after Tuesday -->
+	<a href="quiz.php">Go to Travel Quiz</a>
+	</div>
+	
+<?php
 }else{
 	//incorrect input
-	?><p>Incorrect username/password. Please Try Again.</p>
-	<a href = "login.php">Back to Login</a><?php
+?>
+	<div class="login-form">
+	<p>Incorrect username/password. Please Try Again.</p>
+	<a href = "sign-in.php">Back to Login</a>
+	</div>
+
+<?php
 }
 ?>
+
+
+</body>
+
+</html>
+
