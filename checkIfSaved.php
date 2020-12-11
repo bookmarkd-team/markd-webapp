ac8f684018dd383d6221fee06322e527a70fb856
thisioke <seyitanoke@gmail.com>
2020-12-11 16:56:26 -0500

    progress on the marked function

--- checkIfSaved.php
+++ checkIfSaved.php
@@ -4,16 +4,17 @@
 session_start();
 
 //receive inputs
-$userId = 1; //$_SESSION["userId"]; //who's saved list am I looking for?
+$userId = $_GET["userId"]; //who's saved list am I looking for?
+$destinationId = $_GET["destinationId"]; //who's saved list am I looking for?
 
 //connect to db
 include('includes/dbconfig.php');
 
-$stmt = $pdo->prepare("SELECT `destinationId` FROM `save` WHERE `destinationId` = '$userId' ");
+$stmt = $pdo->prepare("SELECT COUNT(`destinationId`) FROM `save` WHERE `destinationId` = '$destinationId' AND `userId` = '$userId' ");
 
 $stmt->execute();
 
-while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {  
-    echo($row["destinationId"]); //list all the destinations that the selected user has lined
-}
+return $stmt;
+
+
 ?>
