df36f296fb7e2b9a74e763df844a7f6c2e06973e
scdoja <doja@sheridancollege.ca>
2020-12-17 17:51:37 -0500

    REMOVED BIO FROM PROFILE

--- userProfile.php
+++ userProfile.php
@@ -54,9 +54,8 @@ while($results = $stmtLoadUserResults->fetch(PDO::FETCH_ASSOC)) {
 <div id="bio">
 <img src="pig.jpg" height=200 width=200 alt="profile-picture"/> </br>
  <label id="name">Full Name</label> </br>  <!--echo user full name here -->
-<textarea readonly id="bio" name="bio" rows="5" cols="100" > </textarea> </br> <!-- echo user infomation here -->
-
  </div>
+ 
 <div id="buttonArea">
 <a href="editProfile.php?userId=<?php echo ($userId)?>"><button class="button" id="profile">Edit Profile</button></a></br>
 </div>
